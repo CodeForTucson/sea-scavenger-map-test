46461e260c0a43cbd51d286d14851dec7531a16e
Dan Stormont <daniel.stormont@gmail.com>
2020-12-02 23:49:47 -0700

    Fix map to container boundaries and simplify render code.

---
+++ scripts/draw-map.js
@@ -0,0 +1,37 @@
+async function drawMap() {   
+
+    // Get dimensions of map container in current viewport.
+    let mapContainer = document.getElementById("worldmap");
+    let dimensions = {
+        height: mapContainer.offsetHeight,
+        width: mapContainer.offsetWidth
+    };
+    
+    // Retrieve GeoJSON for countries.
+    const countryShapes = await d3.json("data/world-geojson.json");
+
+    // Calculate Equirectangular projection for country data.
+    const projection = d3.geoEquirectangular()
+        .fitSize([dimensions.width, dimensions.height], countryShapes)
+        .center([10, 10]);
+
+    // Generate SVG path for projection.
+    const pathGenerator = d3.geoPath(projection);
+
+    // Create map container.
+    const wrapper = d3.select("#worldmap")
+        .append("svg")
+        .attr("width", dimensions.width)
+        .attr("height", dimensions.height)
+        .style("stroke", "gray")
+        .style("fill", "lightgray");
+
+    // Render map.
+    const countries = wrapper.selectAll(".country")
+        .data(countryShapes.features)
+        .enter()
+        .append("path")
+        .attr("d", pathGenerator);
+}
+
+drawMap();
